da1ee55f5e58875c4aa5bdb727415831d4b254dd
yzhan90top <526342139@qq.com>
2024-03-25 23:18:28 +0800

    First Update

---
+++ Leetcode/ArrayIssue/Longest_SWRC.py
@@ -0,0 +1,4 @@
+#3-8 滑动窗口
+#3 Longest Substring Without Repeating Characters
+#课后题 438 76
+
---
+++ Leetcode/ArrayIssue/MinimumSSS.py
@@ -0,0 +1,54 @@
+#3-7 滑动窗口
+#209 Minimum Size Subarray Sum
+
+class solution(object):
+        
+    def Msss(self, target, nums):
+        if not nums:
+            return 0
+        
+        n = len(nums)
+        l, r = 0, 0
+        sum = 0
+        res = n+1
+        
+        while r < n:
+            sum += nums[r]
+            while sum >= target:
+                res = min(res, r - l + 1)
+                sum -= nums[l]
+                l += 1 
+            r += 1
+    
+        return 0 if res == n+1 else res
+
+    def Msss1(self, target, nums):
+        if not nums:
+            return 0
+        
+        n = len(nums)
+        l, r =0, -1
+        sum = 0
+        res = n+1
+
+        while l < n:
+            if r + 1 < n and sum < target:
+                r += 1
+                sum += nums[r]
+            else:
+                sum -= nums[l]
+                l += 1
+            
+            if sum >= target:
+                res = min (res, r - l +1)
+        
+        return 0 if res == n+1 else res
+
+
+if __name__ == "__main__":
+    
+    s = solution()
+    target = 7
+    nums = [2,3,1,2,4,3]
+    print(s.Msss1(target,nums))
+
